a6c8fc066d87402e1223ca25f1e005860b65a894
Edip SACİ <saciedip@gmail.com>
2023-07-30 11:45:48 +0300

    Initial commit

---
+++ src/test/java/com/demoQAUI/pages/ProfilePage.java
@@ -0,0 +1,15 @@
+package com.demoQAUI.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class ProfilePage extends HomePage{
+
+    @FindBy(xpath = "//span//a")
+    public List<WebElement> addedBooks;
+
+    @FindBy(xpath = "(//button[.='Delete All Books'])[1]")
+    public WebElement deleteAllBooks;
+}
